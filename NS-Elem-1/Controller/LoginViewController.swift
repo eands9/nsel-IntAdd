84c0dc373d4ae15de0e58828f908d1ae1ffa3bf4
Kyle Hernandez <kylehernandez@kyles-macbook-air.local>
2019-02-19 18:50:24 -0600

    before cocoapods and after log in page

---
+++ NS-Elem-1/Controller/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  nsel-IntAdd
+//
+//  Created by Kyle Hernandez on 2/19/19.
+//  Copyright © 2019 Eric Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
